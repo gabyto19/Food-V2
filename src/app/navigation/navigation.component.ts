bd70de98d7223e915748031b56930774618c4af5
gabyto19 <gabuniagoga19@gmail.com>
2024-05-20 19:17:28 +0400

    ----

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  styleUrl: './navigation.component.css',
 })
 export class NavigationComponent {
+  saver:any;
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log(event.urlAfterRedirects);
+        this.saver = event.urlAfterRedirects;
+      }
+    });
+  }
 }
